c60acd0186336e50fd1a0b7e9e4675ffae78aa49
Semyon Fomin <taxnuke@gmail.com>
2017-04-12 20:09:39 +0300

    T-RS trigger VHDL

--- lab10/trstr_vhdl.vhd
+++ lab10/trstr_vhdl.vhd
@@ -2,22 +2,24 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity trstr_vhdl is
-port(t,s,r:in std_logic;
-	q,qi:out std_logic);
+port(t,s,r :in std_logic;
+	 q,qi  :out std_logic);
 end trstr_vhdl;
 
 architecture behaviour of trstr_vhdl is
-signal Qtemp:std_logic;
+	signal Qtemp:std_logic := 'X';
 begin
-	process(t, r, s)
-	begin
+q<=Qtemp;
+qi<=not Qtemp;
+process(t, r, s)
+begin
+	--wait until r = '0';
 	if r='0' then
-	Qtemp<='0';
+		Qtemp<='0';
 	elsif (t'event and t='1') then
-	Qtemp<=not(Qtemp);
+		Qtemp<=not(Qtemp);
 	end if;
 
-	q<=Qtemp;
-	qi<=not(Qtemp);
-	end process;
+end process;
 end behaviour;
+
