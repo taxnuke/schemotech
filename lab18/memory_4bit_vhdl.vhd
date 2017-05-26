e546d38eb34c9205148f74a0ce4aaa7c51bfadc1
Semyon Fomin <taxnuke@gmail.com>
2017-05-27 01:23:44 +0300

    Finish lab4

--- lab18/memory_4bit_vhdl.vhd
+++ lab18/memory_4bit_vhdl.vhd
@@ -11,21 +11,23 @@ END memory_4bit_vhdl;
 
 ARCHITECTURE behavior OF memory_4bit_vhdl IS
 BEGIN
-PROCESS(d, c, a)
-BEGIN
-	IF (c'event and c='1') THEN
-		IF(a(0)='0' and a(1)='0') THEN
-			q(0) <= d;
-		END IF;
-		IF(a(0)='1' and a(1)='0') THEN
-			q(2) <= d;
-		END IF;
-		IF(a(0)='0' and a(1)='1') THEN
-			q(1) <= d;
-		END IF;
-		IF(a(0)='1' and a(1)='1') THEN
-			q(3) <= d;
-		END IF;
-	END IF;
-END PROCESS;
+
+    PROCESS(d, c, a)
+    BEGIN
+    	IF (c'event and c='1') THEN
+    		IF(a(0)='0' and a(1)='0') THEN
+    			q(0) <= d;
+    		END IF;
+    		IF(a(0)='1' and a(1)='0') THEN
+    			q(2) <= d;
+    		END IF;
+    		IF(a(0)='0' and a(1)='1') THEN
+    			q(1) <= d;
+    		END IF;
+    		IF(a(0)='1' and a(1)='1') THEN
+    			q(3) <= d;
+    		END IF;
+    	END IF;
+    END PROCESS;
+
 END behavior;
